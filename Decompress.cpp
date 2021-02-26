883f700eece54d86e4d27729c7eef33c921a3645
Dominik Köppl <dominik.koeppl@uni-dortmund.de>
2021-02-26 13:00:20 +0900

    add option to read solca; add decompress for debug

--- Decompress.cpp
+++ Decompress.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <queue>
+#include <thread>
 #include "cmdline.h"
 #include "Common.hpp"
 #include "PlainSlp.hpp"
@@ -23,78 +24,24 @@ using timer = std::chrono::high_resolution_clock;
 
 using var_t = uint32_t;
 
-
-template
-<
-  class SlpT
-  >
+template<class SlpT>
 void measure
 (
- const NaiveSlp<var_t> & slp,
+ std::string in,
  std::string out
 ) {
+  SlpT slp;
+
   auto start = timer::now();
-  SlpT sslp(slp);
+  ifstream fs(in);
+  slp.load(fs);
   auto stop = timer::now();
-  cout << "time to encode (ms): " << duration_cast<milliseconds>(stop-start).count() << endl;
-  sslp.printStatus();
-
-  start = timer::now();
-  ofstream fs(out);
-  sslp.serialize(fs);
-  stop = timer::now();
-  cout << "time to serialize (ms): " << duration_cast<milliseconds>(stop-start).count() << endl;
-}
-
-
-template
-<
-  class SlpT
-  >
-void measure_PoSlp
-(
- const NaiveSlp<var_t> & slp,
- std::string out
-) {
-  NaiveSlp<var_t> temp(slp);
-  auto start = timer::now();
-  temp.makeBinaryTree();
-  SlpT poslp;
-  poslp.makePoSlpWithLen(temp);
-  auto stop = timer::now();
-  cout << "time to encode (ms): " << duration_cast<milliseconds>(stop-start).count() << endl;
-  poslp.printStatus();
-
-  start = timer::now();
-  ofstream fs(out);
-  poslp.serialize(fs);
-  stop = timer::now();
-  cout << "time to serialize (ms): " << duration_cast<milliseconds>(stop-start).count() << endl;
-}
-
-template
-<
-  class SlpT
-  >
-void measure_PlainSlp
-(
- const NaiveSlp<var_t> & slp,
- std::string out
-) {
-  NaiveSlp<var_t> temp(slp);
-  auto start = timer::now();
-  temp.makeBinaryTree();
-  SlpT pslp;
-  pslp.init(temp);
-  auto stop = timer::now();
-  cout << "time to encode (ms): " << duration_cast<milliseconds>(stop-start).count() << endl;
-  pslp.printStatus();
-
-  start = timer::now();
-  ofstream fs(out);
-  pslp.serialize(fs);
-  stop = timer::now();
-  cout << "time to serialize (ms): " << duration_cast<milliseconds>(stop-start).count() << endl;
+  cout << "time to load (ms): " << duration_cast<milliseconds>(stop-start).count() << endl;
+  // slp.printStatus();
+  std::unique_ptr<char[]> str(new char[slp.getLen()]);
+  slp.expandSubstr(0, slp.getLen(), str.get());
+  ofstream os(out);
+  os << str.get();
 }
 
 
@@ -110,21 +57,20 @@ int main(int argc, char* argv[])
   using funcs_type = map<string,
                          void(*)
                          (
-                          const NaiveSlp<var_t> & slp,
-                          std::string out
+                          std::string in, std::string out
                           )>;
   funcs_type funcs;
 
   //// PlainSlp
-  funcs.insert(make_pair("PlainSlp_FblcFblc", measure_PlainSlp<PlainSlp<var_t, Fblc, Fblc>>));
-  funcs.insert(make_pair("PlainSlp_IblcFblc", measure_PlainSlp<PlainSlp<var_t, IncBitLenCode, Fblc>>));
-  funcs.insert(make_pair("PlainSlp_32Fblc", measure_PlainSlp<PlainSlp<var_t, FixedBitLenCode<32>, Fblc>>));
+  funcs.insert(make_pair("PlainSlp_FblcFblc", measure<PlainSlp<var_t, Fblc, Fblc>>));
+  funcs.insert(make_pair("PlainSlp_IblcFblc", measure<PlainSlp<var_t, IncBitLenCode, Fblc>>));
+  funcs.insert(make_pair("PlainSlp_32Fblc", measure<PlainSlp<var_t, FixedBitLenCode<32>, Fblc>>));
 
   //// PoSlp: Post-order SLP
   //// Sometimes PoSlp_Sd is better than PoSlp_Iblc
-  funcs.insert(make_pair("PoSlp_Iblc", measure_PoSlp<PoSlp<var_t, IncBitLenCode>>));
-  funcs.insert(make_pair("PoSlp_Sd", measure_PoSlp<PoSlp<var_t, DagcSd>>));
-  // funcs.insert(make_pair("PoSlp_Mcl", measure_PoSlp<PoSlp<var_t, DagcMcl>>));
+  funcs.insert(make_pair("PoSlp_Iblc", measure<PoSlp<var_t, IncBitLenCode>>));
+  funcs.insert(make_pair("PoSlp_Sd", measure<PoSlp<var_t, DagcSd>>));
+  // funcs.insert(make_pair("PoSlp_Mcl", measure<PoSlp<var_t, DagcMcl>>));
 
   //// ShapedSlp: plain implementation of slp encoding that utilizes shape-tree grammar
   //// Since bit length to represent slp element is small, SelMcl is good for them.
@@ -134,8 +80,8 @@ int main(int argc, char* argv[])
 
   //// ShapedSlpV2: all vlc vectors are merged into one.
   //// Generally encoding size gets worse than ShapedSlp_SdMclSd_SdMcl because
-  //// - bit length to represnet stg and bal element is large and DagcSd is a good choice, whereas
-  //// - bit size to represent slp element is significantly small and DagcMcl should be used
+  //// - Since bit length to represnet stg and bal element is large, DagcSd is a good choice.
+  //// - On the other hand, bit size to represent slp element is significantly small, and so SelMcl should be used
   funcs.insert(make_pair("ShapedSlpV2_Sd_SdMcl", measure<ShapedSlpV2<var_t, DagcSd, SelSd, SelMcl>>));
   // funcs.insert(make_pair("ShapedSlpV2_SdSdSd", measure<ShapedSlp<var_t, DagcSd, SelSd, SelSd>>));
   // funcs.insert(make_pair("ShapedSlpV2_SdMclMcl", measure<ShapedSlp<var_t, DagcSd, SelMcl, SelMcl>>));
@@ -152,10 +98,6 @@ int main(int argc, char* argv[])
   funcs.insert(make_pair("SelfShapedSlpV2_SdSd_Sd", measure<SelfShapedSlpV2<var_t, DagcSd, DagcSd, SelSd>>));
   // funcs.insert(make_pair("SelfShapedSlpV2_SdSd_Mcl", measure<SelfShapedSlpV2<var_t, DagcSd, DagcSd, SelMcl>>));
 
-
-  //// ShapedSlp_Status_SdMclSd_SdMcl:
-  funcs.insert(make_pair("ShapedSlp_Status_SdMclSd_SdMcl", measure<ShapedSlp_Status<var_t, DagcSd, DagcMcl, DagcSd, SelSd, SelMcl>>));
-
   string methodList;
   for (auto itr = funcs.begin(); itr != funcs.end(); ++itr) {
     methodList += itr->first + ". ";
@@ -163,47 +105,38 @@ int main(int argc, char* argv[])
 
 
   cmdline::parser parser;
-  parser.add<string>("input", 'i', "input file name. <input>.C and <input>.R in Navarro's RePair format", true);
-  parser.add<string>("output", 'o', "output file to which data structure is written", true);
-  parser.add<string>("format", 'f', "format of input: NavarroRepair | Bigrepair | rrepair", true);
+  parser.add<string>("input", 'i', "input file name in which ShapedSlp data structure is written.", true);
+  parser.add<string>("output", 'o', "the decompressed file.", true);
   parser.add<string>("encoding", 'e', "encoding: " + methodList, true);
+
+
+  parser.add<bool>("dummy_flag", 0, "this is dummy flag to prevent that optimization deletes codes", false, false);
   parser.parse_check(argc, argv);
   const string in = parser.get<string>("input");
   const string out = parser.get<string>("output");
-  const string format = parser.get<string>("format");
   const string encoding = parser.get<string>("encoding");
+  const bool dummy_flag = parser.get<bool>("dummy_flag");
 
-  NaiveSlp<var_t> slp;
-  if (format.compare("NavarroRepair") == 0) {
-    slp.load_NavarroRepair(in.data());
-  } else if (format.compare("Bigrepair") == 0) {
-    slp.load_Bigrepair(in.data(),false);
-  } else if (format.compare("rrepair") == 0) {
-    slp.load_Bigrepair(in.data(),true);
-  } else if (format.compare("solca") == 0) {
-    slp.load_solca(in.data());
-  } else {
-    cerr << "error: specify a valid format name: NavarroRepair. Bigrepair" << endl;
-    exit(1);
-  }
+
 
   if (encoding.compare("All") == 0) {
     for (auto itr = funcs.begin(); itr != funcs.end(); ++itr) {
-      if ((itr->first).compare("ShapedSlp_Status_SdMclSd_SdMcl") != 0) {
-        cout << itr->first << ": BEGIN" << std::endl;
-        itr->second(slp, out + itr->first);
-        cout << itr->first << ": END" << std::endl;
-      }
+      cout << itr->first << ": BEGIN" << std::endl;
+      itr->second(in + itr->first, out);
+      cout << itr->first << ": END" << std::endl;
     }
   } else {
     auto itr = funcs.find(encoding);
     if (itr != funcs.end()) {
-      itr->second(slp, out);
+      cout << itr->first << ": BEGIN" << std::endl;
+      itr->second(in, out);
+      cout << itr->first << ": END" << std::endl;
     } else {
-      cerr << "error: specify a valid encoding name: " + methodList << endl;
+      cerr << "error: specify a valid encoding name in " + methodList << endl;
       exit(1);
     }
   }
+
 
   return 0;
 }
--- NaiveSlp.hpp
+++ NaiveSlp.hpp
@@ -55,6 +55,38 @@ public:
    ) : seq_(other.seq_), rules_(other.rules_), alph_(other.alph_) {
   }
 
+  void load_solca(const char* fname_base) {
+    constexpr size_t alphSize = 256; // [0,256) are terminals
+    alph_.resize(alphSize);
+    for (uint64_t i = 0; i < alph_.size(); ++i) {
+      alph_[i] = i;
+    }
+
+	std::ifstream ifs(fname_base);
+    if(!ifs) {
+      fprintf(stderr, "Error: cannot stat file %s\n", fname_base);
+      exit(1);
+    }
+	ifs.seekg(0, std::ios::end);
+	uint64_t eofpos = ifs.tellg();
+	ifs.clear();
+	ifs.seekg(0, std::ios::beg);
+	uint64_t begpos = ifs.tellg();
+	uint64_t num_rules = (eofpos - begpos) / (sizeof(uint32_t) * 2);
+	rules_.resize(num_rules);
+	// left_.resize(num_rules);
+	// right_.resize(num_rules);
+	for (uint64_t i = 0; i < num_rules; ++i)
+	{
+      uint32_t left, right;
+	  ifs.read((char *)&left, sizeof(left));
+	  ifs.read((char *)&right, sizeof(right));
+          rules_[i].left = left;
+          rules_[i].right = right;
+	}
+    seq_.resize(1);
+    seq_[0] = rules_.size() + alphSize -1;
+  }
 
   void load_NavarroRepair
   (
